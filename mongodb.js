594800c9837e3910c02bce395d22118e34c9c347
zephyrus21 <pandey.piyus@gmail.com>
2021-04-02 16:31:23 +0530

    mongoose init

--- mongodb.js
+++ mongodb.js
@@ -1,16 +1,35 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+MongoClient.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) return console.log('Unable to connect')
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({ name: 'Piyush Pandey', age: 22 }, (err, res) => {
-        if (err) return console.log('Unable to insert')
-        console.log(res.ops);
-    })
+    //! Insert one document into a collection
+    // db.collection('users').insertOne({ name: 'Piyush Pandey', age: 22 }, (err, res) => {
+    //     if (err) return console.log('Unable to insert')
+    //     console.log(res.ops);
+    // })
+
+    //! Insert many document into a collection
+    // db.collection('users').insertMany([
+    //     { name: 'Neha', age: 19 },
+    //     { name: 'Billu', age: 32 }
+    // ], (err, res) => {
+    //     if (err) return console.log('Unable to insert')
+    //     console.log(res.ops)
+    // })
+
+    //! Read document from a collection
+    // db.collection('users').findOne({ name: 'Tillu' }).then((res) => console.log(res)).catch((err) => console.log(err))
+
+    //! Update document in a collection
+    // db.collection('users').updateOne({ name: 'Billu' }, { $set: { name: 'Tillu' } }).then((res) => {
+    //     console.log(res)
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
 })
---
+++ src/db/mongoose.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+});
+
+const me = new User({
+    name: 'Piyush',
+    age: 21,
+});
+
+me.save()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
