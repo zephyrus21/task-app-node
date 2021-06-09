12ba39d7b59bde4fc324d098f2d7460b8d633842
zephyrus <pandey.piyus@gmail.com>
2021-06-09 17:42:40 +0530

    uploading files

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middlewares/auth');
 const router = express.Router();
@@ -115,5 +116,29 @@ router.delete('/users/me', auth, async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+//! Uploading Image
+const upload = multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000, //? 1MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg)$/))
+      return cb(new Error('Please upload image'));
+
+    cb(undefined, true);
+  },
+});
+router.post(
+  '/users/me/avatar',
+  upload.single('avatar'),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
